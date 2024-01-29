f092392175fa862d9a68504c4b04dbef27e6d0a2
Cheny1m <849792469@qq.com>
2024-01-29 23:32:24 +0800

    1

--- apss/inference/get_parameter.py
+++ apss/inference/get_parameter.py
@@ -5,7 +5,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     # 添加参数
-    parser.add_argument('-n', '--number_of_nodes', type=int, help='Number of nodes', default=1)
+    parser.add_argument('-n', '--number_of_nodes', type=int, help='Number of nodes', default=2)
     parser.add_argument('-m', '--number_of_GPU_per_node', type=int,help='number of GPU per node',default=2)
     parser.add_argument('--hidden_size',type=int, help='hidden size of the model',default=1024)
     parser.add_argument('--sequence_length', type=int, help='sequence length of the model',default=1024)
